9d9a06ecd06d6894991f93b2b5c6aeda85aa7db6
Tuan Tran <vantuan5644@gmail.com>
2020-04-27 02:31:24 +0700

    Crawling works with 1 CSS pattern, the others still not included

--- src/run_scraper.py
+++ src/run_scraper.py
@@ -1,6 +1,6 @@
 import os
 from urllib.parse import urlparse
-
+import numpy as np
 import scrapy
 from scrapy import signals
 from scrapy.crawler import CrawlerProcess
@@ -69,16 +69,41 @@ class LungCancerPipeline(object):
 
 if __name__ == "__main__":
     # stages = map(lambda x: f'"stage {x}"', range(5))
+    """
+    There are some "hyperparameters" (some kind of hard-coding things) that can affect the results:
+    1. The keyword (search term) to get input URLs from Google, i.e: stage 0
+    2. The keyword again, but in other forms, i.e: stage 3A or stage IIIA
+    3. CSS tag to get the content:
+      - Type 1: <ul> <li>
+      - Type 2: <h2> <p>
+    """
     stages_map = {'stage 0': ['stage 0'],
+                  'stage 1': ['stage 1', 'stage I'],
+                  'stage 2': ['stage 2', 'stage II'],
+                  'stage 3a': ['stage 3a', 'stage IIIA'],
+                  'stage 3b': ['stage 3b', 'stage IIIB'],
+                  'stage 4': ['stage 4', 'stage IV'],
                   }
+    # The other forms of the keyword
+
     for i, (keyword, stages) in enumerate(stages_map.items()):
         csv_file = f'data_crawled/{keyword}.csv'
         if os.path.exists(csv_file):
             os.remove(csv_file)
-        start_urls, titles, snippets = get_google_search_results(search_term=f"non small cell lung cancer treatment by stage")
+
+        final_urls, final_titles, final_snippets = [[]] * 3
+
+        search_terms = [f'non small lung cancer treatment {np.random.choice(stages)}',
+                        'non small lung cancer treatment by stage'
+                        ]
+        for search_term in search_terms:
+            urls, titles, snippets = get_google_search_results(search_term=search_term)
+            final_urls = list(sorted(set(final_urls + urls)))
+            final_titles = list(sorted(set(final_titles + titles)))
+            final_snippets = list(sorted(set(final_snippets + snippets)))
         process = CrawlerProcess(settings={'FEED_FORMAT': 'csv',
                                            'FEED_URI': csv_file,
                                            'ROBOTSTXT_OBEY': False})
 
-        process.crawl(BigLungCancer, start_urls=start_urls, titles=titles, snippets=snippets, stages=stages)
-    process.start()  # the script will block here until the crawling is finished
+        process.crawl(BigLungCancer, start_urls=final_urls, titles=final_titles, snippets=final_snippets, stages=stages)
+    process.start()  # The script will block here until the crawling is finished
